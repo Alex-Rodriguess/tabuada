ac28dbb1a62c99eca213f0291ad82b8a0ba7868e
Alex Rodrigues <alexmusicalsa@gmail.com>
2023-07-19 22:36:38 -0300

    estilos

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let tabuada = '';
     
 
     for(let count=1; count<=10; count++)
-    tabuada += num+ " X " +count+ " = " +num*count+"<br />";
+    tabuada += num + " X " +count+ " = " + num * count + "<br />";
 
     if(num || null){
         resposta.innerHTML = tabuada;
@@ -17,5 +17,27 @@ let tabuada = '';
     }
     
 }
-        
+    
+let tela = document.querySelectorAll('main')
+let btnDark = document.querySelector('#btdark')
+let btnLight = document.querySelector('#btlight')
+
+btnDark.addEventListener('click', modoDark);
+btnLight.addEventListener('click', modoLight);
+
+function modoDark() {
+    console.log("modoDark")
+    tela.classList.add("dark");
+    tela.classList.remove("light");
+ 
+}
+
+function modoLight() {
+    console.log("modoLight")
+    tela.classList.add("light");
+    tela.classList.remove("dark");
+
+}
+
+
     
